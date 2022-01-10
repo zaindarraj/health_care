c3e61d55f41afe6a40f7484cb7a0ccd0b86c4b50
zaindarraj <zaindarraj@gmail.com>
2022-01-10 06:59:41 +0200

    edit

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -27,33 +27,15 @@ class _LogInState extends State<LogIn> {
         title: const Text("Sign In"),
         centerTitle: true,
       ),
-      body: BlocConsumer<RegisterBloc, RegisterState>(
-        listener: (context, state) {
-          if (state is ErrorState) {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text(state.message)));
-            BlocProvider.of<RegisterBloc>(context).add(Await());
-          } else if (state is Registed) {
-            if (state.aType == "medical staff") {
-              try {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                        builder: (_) => MultiBlocProvider(providers: [
-                              BlocProvider.value(
-                                value: BlocProvider.of<RegisterBloc>(context),
-                              ),
-                              BlocProvider(
-                                create: (context) => MedicalNavigationBloc(
-                                    userId: state.user.uid)
-                                  ..add(GoPatientsList()),
-                              ),
-                            ], child: const MedicalStaff())),
-                    (route) => false);
-              } catch (e) {
-                print(e);
-              }
-            } else {
+      body:
+          BlocConsumer<RegisterBloc, RegisterState>(listener: (context, state) {
+        if (state is ErrorState) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.message)));
+          BlocProvider.of<RegisterBloc>(context).add(Await());
+        } else if (state is Registed) {
+          if (state.aType == "medical staff") {
+            try {
               Navigator.pushAndRemoveUntil(
                   context,
                   MaterialPageRoute(
@@ -63,76 +45,98 @@ class _LogInState extends State<LogIn> {
                             ),
                             BlocProvider(
                               create: (context) =>
-                                  PatientBloc(userId: state.user.uid)
-                                    ..add(GoHome()),
+                                  MedicalNavigationBloc(userId: state.user.uid)
+                                    ..add(GoPatientsList()),
                             ),
-                          ], child: const Patient())),
+                          ], child: const MedicalStaff())),
                   (route) => false);
+            } catch (e) {
+              print(e);
             }
+          } else {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => MultiBlocProvider(providers: [
+                          BlocProvider.value(
+                            value: BlocProvider.of<RegisterBloc>(context),
+                          ),
+                          BlocProvider(
+                            create: (context) =>
+                                PatientBloc(userId: state.user.uid)
+                                  ..add(GoHome()),
+                          ),
+                        ], child: const Patient())),
+                (route) => false);
           }
-        },
-        builder: (context, state) {
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                height: MediaQuery.of(context).size.height * 0.4,
-                decoration: boxDecoration(Colors.blue),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Form(
-                        child: TextFormField(
-                          autovalidateMode: AutovalidateMode.always,
-                          validator: validateEmail,
-                          controller: emailC,
-                          decoration: inputDecoration("Email"),
-                        ),
+        }
+      }, builder: (context, state) {
+        if (state is Waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: boxDecoration(Colors.blue),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Form(
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        validator: validateEmail,
+                        controller: emailC,
+                        decoration: inputDecoration("Email"),
                       ),
-                      TextFormField(
-                        obscureText: true,
-                        controller: passwordC,
-                        decoration: inputDecoration("Password"),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            if (emailC.text.isNotEmpty &&
-                                passwordC.text.isNotEmpty) {
-                              BlocProvider.of<RegisterBloc>(context).add(
-                                  LogInEvent(
-                                      email: emailC.text
-                                          .replaceAll(RegExp(r"\s+"), ""),
-                                      password: passwordC.text));
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text("Please fill all fields")));
-                            }
-                          },
-                          child: const Text("Sign In")),
-                      TextButton(
-                          onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (_) => BlocProvider.value(
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      controller: passwordC,
+                      decoration: inputDecoration("Password"),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          if (emailC.text.isNotEmpty &&
+                              passwordC.text.isNotEmpty) {
+                            BlocProvider.of<RegisterBloc>(context).add(
+                                LogInEvent(
+                                    email: emailC.text
+                                        .replaceAll(RegExp(r"\s+"), ""),
+                                    password: passwordC.text));
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text("Please fill all fields")));
+                          }
+                        },
+                        child: const Text("Sign In")),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => BlocProvider.value(
                                         value: BlocProvider.of<RegisterBloc>(
-                                            context),child: const PasswordReset(),)));
-                           
-                            
-                          },
-                          child: const Text("Reset Password"))
-                    ],
-                  ),
+                                            context),
+                                        child: const PasswordReset(),
+                                      )));
+                        },
+                        child: const Text("Reset Password"))
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      }),
     );
   }
 }
